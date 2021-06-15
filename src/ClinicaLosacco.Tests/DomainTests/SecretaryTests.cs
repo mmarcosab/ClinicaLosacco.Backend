4c8097d6a6ee99f69ebe4d42d8cd38f7e190d272
Marcos Vinicius <marcosab.vinicius@gmail.com>
2021-06-15 02:10:29 -0300

    ajustes

--- src/ClinicaLosacco.Tests/DomainTests/SecretaryTests.cs
+++ src/ClinicaLosacco.Tests/DomainTests/SecretaryTests.cs
@@ -28,11 +28,51 @@ namespace ClinicaLosacco.Tests.DomainTests
         }
 
         [Fact]
-        public void criarHealthPlanFailWithNo()
+        public void criarHealthPlanFailWithNoEmail()
         {
             Address address = new Address("Rua de Teste", 250, "Apartamento 10", "Sao Paulo", "SP", "01234567", "Brasil");
             Secretary secretary;
-            Assert.Throws<ArgumentException>(() => secretary = new Secretary("Maria", "teste@teste.com.br", "13123456789", address));
+            Assert.Throws<ArgumentException>(() => secretary = new Secretary("Maria", "", "13123456789", address));
+        }
+
+        [Fact]
+        public void criarHealthPlanFailWithNoPhoneNumber()
+        {
+            Address address = new Address("Rua de Teste", 250, "Apartamento 10", "Sao Paulo", "SP", "01234567", "Brasil");
+            Secretary secretary;
+            Assert.Throws<ArgumentException>(() => secretary = new Secretary("Maria", "teste@teste.com.br", "", address));
+        }
+
+        [Fact]
+        public void criarHealthPlanFailWithNullName()
+        {
+            Address address = new Address("Rua de Teste", 250, "Apartamento 10", "Sao Paulo", "SP", "01234567", "Brasil");
+            Secretary secretary;
+            Assert.Throws<ArgumentException>(() => secretary = new Secretary(null, "teste@teste.com.br", "13123456789", address));
+        }
+
+        [Fact]
+        public void criarHealthPlanFailWithNullEmail()
+        {
+            Address address = new Address("Rua de Teste", 250, "Apartamento 10", "Sao Paulo", "SP", "01234567", "Brasil");
+            Secretary secretary;
+            Assert.Throws<ArgumentException>(() => secretary = new Secretary("Maria", null, "13123456789", address));
+        }
+
+        [Fact]
+        public void criarHealthPlanFailWithNullPhoneNumber()
+        {
+            Address address = new Address("Rua de Teste", 250, "Apartamento 10", "Sao Paulo", "SP", "01234567", "Brasil");
+            Secretary secretary;
+            Assert.Throws<ArgumentException>(() => secretary = new Secretary("Maria", "teste@teste.com.br", null, address));
+        }
+
+        [Fact]
+        public void criarHealthPlanFailWithNullAddress()
+        {
+            Address address = new Address("Rua de Teste", 250, "Apartamento 10", "Sao Paulo", "SP", "01234567", "Brasil");
+            Secretary secretary;
+            Assert.Throws<ArgumentNullException>(() => secretary = new Secretary("Maria", "teste@teste.com.br", "13123456789", null));
         }
 
     }
