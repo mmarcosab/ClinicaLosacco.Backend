f88e55c4a9fa2b9532cc5723a4a4a96105b69122
Marcos Vinicius <marcosab.vinicius@gmail.com>
2021-06-20 23:59:46 -0300

    add healthcheck

---
+++ src/ClinicaLosacco.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaLosacco.API.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        public string healthcheck()
+        {
+            return "200 OK";
+        }
+    }
+}
